76e8c7e26972f05455fd430c1aa796514a35f57a
Arthur <arthur@cloudawesome.uk>
2025-06-24 18:12:33 +0100

    Initial docusaurus configuration

---
+++ src/northumbriana/src/pages/index.tsx
@@ -0,0 +1,45 @@
+import type {ReactNode} from 'react';
+import clsx from 'clsx';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Heading from '@theme/Heading';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div>
+          <p>
+            Northumberland is England's northernmost county, bounded to the East by the North Sea, to the North by Scotland, to the west by the county of Cumbria, and to the South by County Durham.
+          </p>
+          <p>
+            Its history, heritage and culture reflect the diversity of landscapes, occupations and lifestyles which have evolved over many centuries.
+          </p>
+        </div>
+      </div>
+
+    </header>
+  );
+}
+
+export default function Home(): ReactNode {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
